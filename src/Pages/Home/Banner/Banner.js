f3277fd4f02ea2431118b35ef40f769123061bfc
noyon-podder <noyon.vai369@gmail.com>
2022-11-24 01:51:36 +0600

    banner section add this commit

---
+++ src/Pages/Home/Banner/Banner.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import banner from '../../../images/banner.jpg';
+const Banner = () => {
+    return (
+        <div style={{ 
+            backgroundImage: `url(${banner})` ,
+            backgroundSize: 'cover',
+            backgroundPosition: 'center',
+            backgroundRepeat: 'no-repeat',
+          }} className='h-screen relative'>
+            <div 
+            style={{
+                position: 'absolute',
+                top: '50%',
+                left: '80px',
+                transform: 'translateY(-50%)' 
+            }}
+            className='max-w-7xl mx-auto w-1/2'>
+            <span className='bg-primary uppercase font-semibold text-white p-2'> We are here for you</span>
+
+            <h2 className='text-7xl my-5 text-white font-semibold  capitalize'>Improve your business with us</h2>
+            <h6 className='text-white font-normal text-xl w-3/4 mt-5'>We provide the best solution for your car needs.We offer great vehicle round the clock.</h6>
+            <button className='btn btn-primary px-8 mt-8'>Start Now</button>
+            </div>
+        </div>
+    );
+};
+
+export default Banner;
--- src/Pages/shared/Footer/Footer.js
+++ src/Pages/shared/Footer/Footer.js
@@ -1,10 +1,36 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Footer = () => {
     return (
-        <div>
-            <h3>this is footer</h3>
-        </div>
+        <footer className=" bg-neutral">
+   <div className='p-10 footer text-white max-w-7xl mx-auto'>
+  <div>
+  <Link to="/"className="text-2xl font-bold text-white uppercase ">car<span className='text-primary'>Dealer</span></Link>
+    <p>ACME Industries Ltd.<br/>Providing reliable tech since 1992</p>
+  </div> 
+  <div>
+    <span className="footer-title">Services</span> 
+    <Link className="link link-hover">Branding</Link> 
+    <Link className="link link-hover">Design</Link> 
+    <Link className="link link-hover">Marketing</Link> 
+    <Link className="link link-hover">Advertisement</Link>
+  </div> 
+  <div>
+    <span className="footer-title">Company</span> 
+    <Link className="link link-hover">About us</Link> 
+    <Link className="link link-hover">Contact</Link> 
+    <Link className="link link-hover">Jobs</Link> 
+    <Link className="link link-hover">Press kit</Link>
+  </div> 
+  <div>
+    <span className="footer-title">Legal</span> 
+    <Link className="link link-hover">Terms of use</Link> 
+    <Link className="link link-hover">Privacy policy</Link> 
+    <Link className="link link-hover">Cookie policy</Link>
+  </div>
+  </div>
+</footer>
     );
 };
 
